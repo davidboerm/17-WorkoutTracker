02e2af97f0ef1a108ddb5872017cc23727fe038b
david boerm <davidboerm@gmail.com>
2021-07-23 01:59:20 -0500

    deploying

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = 3000;
+
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
 
