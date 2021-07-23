181a987d35765c4f727edb32c868a01e438cb07a
david boerm <davidboerm@gmail.com>
2021-07-23 01:08:55 -0500

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/NoSQL_db', {
+  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
